be4d6f399b853d05ab9e1215f358cca13a3ea76e
William Kent <wjk@sunsol.me>
2016-02-11 14:13:43 -0500

    Reindent bootstruct.c
    I will be making changes to this file next commit,
    and reindenting it now will make the changes easier
    to spot in the diff.

--- libsaio/bootstruct.c
+++ libsaio/bootstruct.c
@@ -2,7 +2,7 @@
  * Copyright (c) 1999-2003 Apple Computer, Inc. All rights reserved.
  *
  * @APPLE_LICENSE_HEADER_START@
- * 
+ *
  * Portions Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights
  * Reserved.  This file contains Original Code and/or Modifications of
  * Original Code as defined in and that are subject to the Apple Public
@@ -10,7 +10,7 @@
  * except in compliance with the License.  Please obtain a copy of the
  * License at http://www.apple.com/publicsource and read it before using
  * this file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,7 +18,7 @@
  * FITNESS FOR A PARTICULAR PURPOSE OR NON- INFRINGEMENT.  Please see the
  * License for the specific language governing rights and limitations
  * under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  */
 /*
@@ -48,40 +48,40 @@ void initKernBootStruct( int biosdev )
     Node *node;
     int nameLen;
     static int init_done = 0;
-
+    
     if ( !init_done )
     {
         bootArgs = (boot_args *)malloc(sizeof(boot_args));
         bootInfo = (PrivateBootInfo_t *)malloc(sizeof(PrivateBootInfo_t));
         if (bootArgs == 0 || bootInfo == 0)
             stop("Couldn't allocate boot info\n");
-
+        
         bzero(bootArgs, sizeof(boot_args));
         bzero(bootInfo, sizeof(PrivateBootInfo_t));
-
+        
         // Get system memory map. Also update the size of the
         // conventional/extended memory for backwards compatibility.
-
+        
         bootInfo->memoryMapCount =
-            getMemoryMap( bootInfo->memoryMap, kMemoryMapCountMax,
-                          (unsigned long *) &bootInfo->convmem,
-                          (unsigned long *) &bootInfo->extmem );
-
+        getMemoryMap( bootInfo->memoryMap, kMemoryMapCountMax,
+                     (unsigned long *) &bootInfo->convmem,
+                     (unsigned long *) &bootInfo->extmem );
+        
         if ( bootInfo->memoryMapCount == 0 )
         {
             // BIOS did not provide a memory map, systems with
             // discontiguous memory or unusual memory hole locations
             // may have problems.
-
+            
             bootInfo->convmem = getConventionalMemorySize();
             bootInfo->extmem  = getExtendedMemorySize();
         }
-
+        
         bootInfo->configEnd    = bootInfo->config;
         bootArgs->Video.v_display = VGA_TEXT_MODE;
         
         DT__Initialize();
-
+        
         node = DT__FindNode("/", true);
         if (node == 0) {
             stop("Couldn't create root node");
@@ -97,17 +97,17 @@ void initKernBootStruct( int biosdev )
         }
         
         DT__AddProperty(efi_node, "FSBFrequency", sizeof(FSBFrequency), &FSBFrequency);
-
+        
         gMemoryMapNode = DT__FindNode("/chosen/memory-map", true);
-
+        
         bootArgs->Version  = kBootArgsVersion;
         bootArgs->Revision = kBootArgsRevision;
-
+        
         init_done = 1;
     }
-
+    
     // Update kernDev from biosdev.
-
+    
     bootInfo->kernDev = biosdev;
 }
 
@@ -131,48 +131,48 @@ finalizeBootStruct(void)
     EfiMemoryRange *memoryMap;
     MemoryRange *range;
     int memoryMapCount = bootInfo->memoryMapCount;
-
+    
     if (memoryMapCount == 0) {
         // XXX could make a two-part map here
         stop("Unable to convert memory map into proper format\n");
     }
-
+    
     // convert memory map to boot_args memory map
     memoryMap = (EfiMemoryRange *)AllocateKernelMemory(sizeof(EfiMemoryRange) * memoryMapCount);
     bootArgs->MemoryMap = memoryMap;
     bootArgs->MemoryMapSize = sizeof(EfiMemoryRange) * memoryMapCount;
     bootArgs->MemoryMapDescriptorSize = sizeof(EfiMemoryRange);
     bootArgs->MemoryMapDescriptorVersion = 0;
-
+    
     for (i=0; i<memoryMapCount; i++, memoryMap++) {
         range = &bootInfo->memoryMap[i];
         switch(range->type) {
-        case kMemoryRangeACPI:
-            memoryMap->Type = kEfiACPIReclaimMemory;
-            break;
-        case kMemoryRangeNVS:
-            memoryMap->Type = kEfiACPIMemoryNVS;
-            break;
-        case kMemoryRangeUsable:
-            memoryMap->Type = kEfiConventionalMemory;
-            break;
-        case kMemoryRangeReserved:
-        default:
-            memoryMap->Type = kEfiReservedMemoryType;
-            break;
+            case kMemoryRangeACPI:
+                memoryMap->Type = kEfiACPIReclaimMemory;
+                break;
+            case kMemoryRangeNVS:
+                memoryMap->Type = kEfiACPIMemoryNVS;
+                break;
+            case kMemoryRangeUsable:
+                memoryMap->Type = kEfiConventionalMemory;
+                break;
+            case kMemoryRangeReserved:
+            default:
+                memoryMap->Type = kEfiReservedMemoryType;
+                break;
         }
         memoryMap->PhysicalStart = range->base;
         memoryMap->VirtualStart = range->base;
         memoryMap->NumberOfPages = range->length >> I386_PGSHIFT;
         memoryMap->Attribute = 0;
     }
-
+    
     // copy bootFile into device tree
     // XXX
-
+    
     // add PCI info somehow into device tree
     // XXX
-
+    
     // Flatten device tree
     DT__FlattenDeviceTree(0, &size);
     addr = (void *)AllocateKernelMemory(size);
